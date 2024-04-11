5ea678fe4fc1070506dff4cdd87ce2aeab5811e1
Alexandre Siljegovic <alexandresiljegovic22@gmail.com>
2024-04-11 15:22:10 +0200

    final

--- src/pages/NoteEditForm/index.js
+++ src/pages/NoteEditForm/index.js
@@ -6,7 +6,8 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   Keyboard,
-  Alert
+  Alert,
+  Text
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import Style from "./style";
@@ -19,6 +20,7 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 export default function NotesEdit({ route, navigation }) {
+  
 
    // Styles pour le sélecteur de valeurs
   const pickerSelectStyles = StyleSheet.create({
@@ -78,6 +80,37 @@ export default function NotesEdit({ route, navigation }) {
     notificationId: null,
     importance: "",
   });
+  const hasUnsavedChanges = Boolean(note.title || note.note || note.importance);
+
+  React.useEffect(
+    () =>
+      navigation.addListener('beforeRemove', (e) => {
+        if (!hasUnsavedChanges) {
+          // If we don't have unsaved changes, then we don't need to do anything
+          return;
+        }
+
+        // Prevent default behavior of leaving the screen
+        e.preventDefault();
+
+        // Prompt the user before leaving the screen
+        Alert.alert(
+          'Discard changes?',
+          'You have unsaved changes. Are you sure to discard them and leave the screen?',
+          [
+            { text: "Don't leave", style: 'cancel', onPress: () => {} },
+            {
+              text: 'Discard',
+              style: 'destructive',
+              // If the user confirmed, then we dispatch the action we blocked earlier
+              // This will continue the action that had triggered the removal of the screen
+              onPress: () => navigation.dispatch(e.data.action),
+            },
+          ]
+        );
+      }),
+    [navigation, hasUnsavedChanges]
+  );
 
 
 
